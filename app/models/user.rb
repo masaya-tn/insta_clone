ef6eb3423dd36695984fb42655d99c8b215b30b2
masaya-tn <uvermasayan@icloud.com>
2021-02-08 00:43:09 +0900

    フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  has_many :active_relationships, class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+           foreign_key: 'follwed_id',
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   scope :randoms, -> (count) { order("RAND()").limit(count) }
 
@@ -44,5 +52,17 @@ class User < ApplicationRecord
 
   def like?(post)
     like_posts.include?(post)
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
